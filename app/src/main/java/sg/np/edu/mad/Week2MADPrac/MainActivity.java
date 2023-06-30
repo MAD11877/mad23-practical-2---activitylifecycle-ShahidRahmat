a3d09ed0b13b2b2ec3b5d1533e058530c8899c70
ShahidRahmat <shahid.rahmats@gmail.com>
2023-06-30 20:45:14 +0800

    Code refractored but reverted back due to refractor causing issues

---
+++ app/src/main/java/sg/np/edu/mad/Week2MADPrac/MainActivity.java
@@ -0,0 +1,89 @@
+package sg.np.edu.mad.Week2MADPrac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import sg.np.edu.mad.week2madprac.R;
+
+public class MainActivity extends AppCompatActivity {
+    final String TITLE = "MAD Practical";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        User user1 = new User("Shahid", "zao shang hao zhong guo", 1, false);
+        Button fbutton = findViewById(R.id.fbutton);
+
+        TextView namebox = findViewById(R.id.namebox);
+        TextView descbox = findViewById(R.id.descbox);
+        namebox.setText(user1.name);
+        descbox.setText(user1.description);
+        Log.v("MainActivity", "MainActivity ran");
+
+        fbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "onclick clicked");
+
+                if (user1.followed == false){
+                    fbutton.setText("FOLLOW");
+                    user1.followed = true;
+                }
+                else {
+                    fbutton.setText("UNFOLLOW");
+                    user1.followed = false;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.v(TITLE, "Start");
+
+    }
+
+    @Override
+    protected void onResume(){
+
+        super.onResume();
+        Log.v(TITLE, "Resume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.v(TITLE, "Paused");
+    }
+
+    @Override
+    protected void onStop(){
+
+        super.onStop();
+        Log.v(TITLE, "Stop");
+    }
+
+    @Override
+    protected void onRestart(){
+
+        super.onRestart();
+        Log.v(TITLE, "Restart");
+    }
+
+   /*@Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.v(TITLE, "Destroy");
+    }*/
+
+}
